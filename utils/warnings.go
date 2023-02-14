aa4a40edd7231994b6005e0f822343e2c5fca4e4
Ramazan Zholdas <ghostcomrad@gmail.com>
2023-02-14 10:58:55 +0600

    warnings changed

--- utils/warnings.go
+++ utils/warnings.go
@@ -5,10 +5,36 @@ import (
 	"os"
 )
 
-func CreateLogFile() {
-	file, err := os.OpenFile("./../../logs/ProgramLogs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+var (
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+	file    *os.File
+)
+
+func CreateLogFile(fileName string) {
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		Error.Fatalln("Failed to open log file:", err)
 	}
-	defer file.Close()
+
+	Info = log.New(file,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(file,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(file,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// CloseLogFile closes the log file.
+func CloseLogFile() {
+	err := file.Close()
+	if err != nil {
+		Error.Fatalln("Failed to close log file:", err)
+	}
 }
